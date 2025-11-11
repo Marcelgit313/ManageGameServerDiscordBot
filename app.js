0150a4ee48129efd578e28874d7501f6ecd8036e
Marcelgit313 <kampfcookie@gmail.com>
2025-11-11 15:38:17 +0100

    chore: some fix

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import 'dotenv/config'
+import 'dotenv/config';
 import {
     InteractionResponseFlags,
     InteractionResponseType,
     InteractionType,
     MessageComponentTypes, verifyKeyMiddleware
 } from "discord-interactions";
+import {getRandomEmoji} from "./utils.js";
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
@@ -18,46 +19,46 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
     // Interaction id, type and data
     const { id, type, data } = req.body;
 
-    /**
-     * Handle verification requests
-     */
-    if (type === InteractionType.PING) {
-        return res.send({ type: InteractionResponseType.PONG });
-    }
-
-    /**
-     * Handle slash command requests
-     * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-     */
-    if (type === InteractionType.APPLICATION_COMMAND) {
-        const { name } = data;
-
-        // "test" command
-        if (name === 'test') {
-            // Send a message into the channel where command was triggered from
-            return res.send({
-                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                data: {
-                    flags: InteractionResponseFlags.IS_COMPONENTS_V2,
-                    components: [
-                        {
-                            type: MessageComponentTypes.TEXT_DISPLAY,
-                            // Fetches a random emoji to send from a helper function
-                            content: `hello world ${getRandomEmoji()}`
-                        }
-                    ]
-                },
-            });
+        /**
+         * Handle verification requests
+         */
+        if (type === InteractionType.PING) {
+            return res.send({type: InteractionResponseType.PONG});
         }
 
-        console.error(`unknown command: ${name}`);
-        return res.status(400).json({ error: 'unknown command' });
-    }
+        /**
+         * Handle slash command requests
+         * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+         */
+        if (type === InteractionType.APPLICATION_COMMAND) {
+            const {name} = data;
 
-    console.error('unknown interaction type', type);
-    return res.status(400).json({ error: 'unknown interaction type' });
-});
+            // "test" command
+            if (name === 'test') {
+                // Send a message into the channel where command was triggered from
+                return res.send({
+                    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    data: {
+                        flags: InteractionResponseFlags.IS_COMPONENTS_V2,
+                        components: [
+                            {
+                                type: MessageComponentTypes.TEXT_DISPLAY,
+                                // Fetches a random emoji to send from a helper function
+                                content: `hello world ${getRandomEmoji()}`
+                            }
+                        ]
+                    },
+                });
+            }
+
+            console.error(`unknown command: ${name}`);
+            return res.status(400).json({error: 'unknown command'});
+        }
+
+        console.error('unknown interaction type', type);
+        return res.status(400).json({error: 'unknown interaction type'});
+    });
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
-})
+});
